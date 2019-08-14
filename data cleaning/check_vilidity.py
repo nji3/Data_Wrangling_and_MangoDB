379a9a9cddc1d1a4db660c408b201c575a5ef88c
nji3 <nji3@g.ucla.edu>
2019-08-14 14:07:24 -0700

    update

--- data cleaning/check_vilidity.py
+++ data cleaning/check_vilidity.py
@@ -24,33 +24,30 @@ OUTPUT_GOOD = 'autos-valid.csv'
 OUTPUT_BAD = 'FIXME-autos.csv'
 
 def process_file(input_file, output_good, output_bad):
+    #COMPLETE THIS FUNCTION
+    good_rows = []
+    bad_rows = []
     with open(input_file, "r") as f:
         reader = csv.DictReader(f)
         header = reader.fieldnames
-        #COMPLETE THIS FUNCTION
-        good_rows = []
-        bad_rows = []
-        with open(input_file, "r") as f:
-            reader = csv.DictReader(f)
-            header = reader.fieldnames
-            for row in reader:
-                time = row['productionStartYear']
-                if time != 'NULL':
+        for row in reader:
+            time = row['productionStartYear']
+            if time != 'NULL':
+                try:
+                    a = time.split("-")
                     try:
-                        a = time.split("-")
-                        try:
-                            year = int(a[0])
-                            if year >= 1886 and year <= 2014:
-                                good_rows.append(row)
-                            else:
-                                bad_rows.append(row)
-                        except ValueError:
+                        year = int(a[0])
+                        if year >= 1886 and year <= 2014:
+                            good_rows.append(row)
+                        else:
                             bad_rows.append(row)
                     except ValueError:
-                        #print(time)
                         bad_rows.append(row)
-                else:
+                except ValueError:
+                    #print(time)
                     bad_rows.append(row)
+            else:
+                bad_rows.append(row)
     # This is just an example on how you can use csv.DictWriter
     # Remember that you have to output 2 files
     # Good
